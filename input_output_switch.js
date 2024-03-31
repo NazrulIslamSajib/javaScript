de88f0e085908f0c41e3103e8f5048963df6a79a
Nazrul Islam Sajib <124718648+NazrulIslamSajib@users.noreply.github.com>
2024-03-31 22:28:11 +0600

    Create input_output_switch.js

---
+++ input_output_switch.js
@@ -0,0 +1,16 @@
+"use strict";
+const ps=require("prompt-sync");
+const prompt=ps();
+var num=prompt("Please Enter you code  :");
+var total=parseInt(num);
+switch(num)
+{
+    case 10:
+        console.log("sajib"); 
+        
+    case 10:
+        console.log("name");
+        break; 
+    default:
+        console.log("konotai mile nai ");
+}
