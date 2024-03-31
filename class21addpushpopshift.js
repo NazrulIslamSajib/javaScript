1c988bbc2076e58dfa4e43784edd5b7b45dd612f
Nazrul Islam Sajib <124718648+NazrulIslamSajib@users.noreply.github.com>
2024-03-31 23:49:31 +0600

    Create class21addpushpopshift.js

---
+++ class21addpushpopshift.js
@@ -0,0 +1,11 @@
+var array=[];
+array.push(1); 
+array.push(2);
+console.log(array[0]);  
+/* 
+  shift dara array er element remove kora hoy. 
+  shift lekha mane shuru theke ekta element bad dite can .  
+*/ 
+array.shift();  
+array.unshift(10);
+console.log(array);
