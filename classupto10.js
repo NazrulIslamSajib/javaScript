6a20f034a53e0b8974b387e6bd2aa3cc885d4d02
Nazrul Islam Sajib <124718648+NazrulIslamSajib@users.noreply.github.com>
2024-03-31 20:10:38 +0600

    Create classupto10.js

---
+++ classupto10.js
@@ -0,0 +1,17 @@
+ 
+var saj="sajib";
+console.log(typeof(saj)); 
+
+var up="SAJIB";
+var ok=true; 
+console.log(ok); 
+console.log(saj.toUpperCase()); 
+console.log(up.toLocaleLowerCase()); 
+var sarray="sajib,nazrul,islam";
+console.log(sarray.split("sajib")); 
+console.log(sarray.indexOf("sajib")); 
+var cvt1="55";
+var cvt2="10";
+var mot=parseInt(cvt1)+parseInt(cvt2);
+console.log(mot); 
+
