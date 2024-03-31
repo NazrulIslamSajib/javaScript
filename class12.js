7606b5bdb9284c2e7e4718f8480d077846bc8889
Nazrul Islam Sajib <124718648+NazrulIslamSajib@users.noreply.github.com>
2024-03-31 20:25:41 +0600

    Create class12.js

---
+++ class12.js
@@ -0,0 +1,13 @@
+var neg=-20; 
+console.log(neg);
+var pos=Math.abs(neg);
+console.log(pos); 
+var r=20.1;
+var roun=Math.ceil(r);
+console.log(roun);  
+
+/* round er floor er modde difference ache. math.round diye .5 er jonno ei dile oidik korbe. 
+*/ 
+var rou=0.6;
+var result=Math.round(rou);
+console.log(result);
